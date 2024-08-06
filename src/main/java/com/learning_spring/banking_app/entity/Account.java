19ebe08e84021931f770ba0d1e743124f451eda6
sandeepThapa520 <sandeepmagar6224@gmail.com>
2024-08-06 20:01:30 +0545

    structured build

--- src/main/java/com/learning_spring/banking_app/entity/Account.java
+++ src/main/java/com/learning_spring/banking_app/entity/Account.java
@@ -1,7 +1,33 @@
 package com.learning_spring.banking_app.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="accounts")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="account_holder_name")
     private String accountHolderName;
+
+    @Column(name="account_holder_locaiton")
+    private String accountHolderLocation;
+
+    @Column(name="account_number")
+    private Long accountNumber;
+
+    @Column(name="account_balance")
+    private Double balance;
 
 }
